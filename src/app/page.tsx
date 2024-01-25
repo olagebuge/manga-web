3b2825fe57bbb05837da140ccd33814c61832314
olagebuge <53190225+olagebuge@users.noreply.github.com>
2024-01-25 15:34:09 +0800

    mock api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { NewStory } from "@/components/newStory";
-import { ApiData } from "@/components/apidata";
+
 const Home = async ({}) => {
   return (
     <section className="flex flex-col items-center mt-5">
@@ -8,8 +8,8 @@ const Home = async ({}) => {
         <h1 className="h1title">最新作品</h1>
         <Link href="#">更多作品</Link>
       </div>
-      {/* <NewStory /> */}
-      <ApiData />
+      <NewStory />
+     
     </section>
   );
 };
